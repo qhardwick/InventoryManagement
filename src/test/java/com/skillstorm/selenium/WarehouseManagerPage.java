b64cf49fc0675cf2e91a1a28f460b3c242b936c0
qhardwick <qhardwick@gmail.com>
2024-09-20 16:16:27 -0400

    changed test to correct url

--- src/test/java/com/skillstorm/selenium/WarehouseManagerPage.java
+++ src/test/java/com/skillstorm/selenium/WarehouseManagerPage.java
@@ -1,5 +1,6 @@
 package com.skillstorm.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,10 +12,10 @@ import java.time.Duration;
 
 public class WarehouseManagerPage {
     private WebDriver driver;
-    private static final String url = "http://localhost:8080/inventory/warehouses";
+    private static final String url = "http://localhost:5173/warehouses";
 
     // Button to open add new warehouse form:
-    @FindBy(css = "button.btn-add")
+    @FindBy(className = "btn-add")
     private WebElement addWarehouseButton;
 
     // Name of the warehouse to be added:
@@ -39,6 +40,7 @@ public class WarehouseManagerPage {
         PageFactory.initElements(driver, this);
     }
 
+    // Navigate to the page:
     public void get() {
         driver.navigate().to(url);
     }
@@ -51,8 +53,9 @@ public class WarehouseManagerPage {
     // Click on the button to open the add new warehouse form:
     public void clickAddWarehouseButton() {
         // Check for visibility before attempting to interact with the form:
+        System.out.println("\n\nclickAddWarehouseButton method called \n\n");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(2000));
-        wait.until(ExpectedConditions.visibilityOf(addWarehouseButton));
+        wait.until(ExpectedConditions.elementToBeClickable(addWarehouseButton));
         addWarehouseButton.click();
     }
 
@@ -70,5 +73,14 @@ public class WarehouseManagerPage {
     // Click on the button to submit the form:
     public void submitForm() {
         submitButton.click();
+    }
+
+    // Verify the Warehouse had been added:
+    public boolean wasWarehouseAdded(String name) {
+        // Use WebDriverWait to ensure the element is interactable
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(2000));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[contains(text(),'" + name + "')]")));
+
+        return driver.findElements(By.xpath("//td[contains(text(),'" + name + "')]")).size() > 0;
     }
 }
