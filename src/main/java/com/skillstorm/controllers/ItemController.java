3be128fcde026ee010b901aef9b0f0fe9ab60813
Tyler <84280214+tylercausey@users.noreply.github.com>
2024-09-24 21:46:32 -0500

    updated CORS to work for AWS endpoints

--- src/main/java/com/skillstorm/controllers/ItemController.java
+++ src/main/java/com/skillstorm/controllers/ItemController.java
@@ -11,7 +11,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173", "http://3.95.37.62", "http://54.226.216.9"})
 @RequestMapping("/items")
 public class ItemController {
 
