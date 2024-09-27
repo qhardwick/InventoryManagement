a25e220880f1d2a72ee14734b0b3fb56c9dec801
Tyler <84280214+tylercausey@users.noreply.github.com>
2024-09-26 23:26:12 -0500

    renamed test executing file, updated pom for unit tests to run, updated controller test to use webmvctest

--- src/test/java/com/skillstorm/controllers/ItemControllerTest.java
+++ src/test/java/com/skillstorm/controllers/ItemControllerTest.java
@@ -13,6 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
 
 import java.util.List;
 
@@ -24,8 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(ItemController.class)
 class ItemControllerTest {
 
     @Autowired
