e34f58cd2ebd9a6d12f1a03065d3837bd9d6f1f5
qhardwick <qhardwick@gmail.com>
2024-09-27 05:18:52 -0400

    add mvctest annotation

--- src/test/java/com/skillstorm/controllers/WarehouseControllerTest.java
+++ src/test/java/com/skillstorm/controllers/WarehouseControllerTest.java
@@ -10,8 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,8 +24,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(ItemController.class)
 class WarehouseControllerTest {
 
     @Autowired
